3c8bad26adbbdefb9694df46f02a96753e10f8b6
YacqubAdan <yacquubadan.1@gmail.com>
2025-03-05 23:47:48 +0000

    fix: wait for eks module

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -21,9 +21,8 @@ provider "helm" {
     cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
     exec {
       api_version = "client.authentication.k8s.io/v1"
-      args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.cluster.name]
+      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
       command     = "aws"
     }
   }
-
 }
