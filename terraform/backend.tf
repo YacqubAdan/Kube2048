44621ba7aec26feb14cce6de9e9d22c34a7f70a8
YacqubAdan <yacquubadan.1@gmail.com>
2025-03-05 22:34:56 +0000

    fix: region

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "eks-proj-backend"
     key            = "eks-argo"
-    region         = "us-west-2"
+    region         = "eu-west-2"
     encrypt        = true
     dynamodb_table = "eks-proj-backend"
   }
